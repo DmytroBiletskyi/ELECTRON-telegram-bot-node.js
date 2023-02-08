750c4996a90934887917e02deb0711119abb889a
Dima <babaha3000@gmail.com>
2023-02-08 22:32:43 +0200

    create calculation option

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import {GoogleSpreadsheet} from "google-spreadsheet";
 import creds from "./client_secret.json" assert {type: "json"};
 
 import pass from "./passwords.json" assert {type: "json"};
-import {keyboardMainMenu, choiceElementButtons, backButton} from "./options.js";
+import {keyboardMainMenu, choiceElementButtons, backButton, choiceConvertButtons, choiceVoltageButtons} from "./options.js";
 
 
 const bot = new TelegramApi(pass.token, {polling: true})
@@ -13,6 +13,10 @@ const bot = new TelegramApi(pass.token, {polling: true})
 let toggle = 0
 let material = 0
 let arr = []
+let choiceConvert = 0
+let U = 0
+
+const parse_mode = {parse_mode: "HTML"}
 
 const start = async () => {
 
@@ -27,27 +31,27 @@ const start = async () => {
 
         if (text === "/start") {
             toggle = 0
-            return bot.sendMessage(chatId, "Вітаю, оберіть якусь дію:", keyboardMainMenu)
+            return bot.sendMessage(chatId, "Вітаю👋, оберіть якусь дію:", keyboardMainMenu)
         }
-        if (text === "Назад") {
+        if (text === "Назад👈") {
             toggle = 0
             await bot.sendMessage(chatId, "Обери наступну дію:", keyboardMainMenu)
         }
-        if (text === "Переріз кабеля" || toggle === 1) {
+        if (text === "Переріз кабеля ➰" || toggle === 1) {
             if (toggle === 1) {
                 let S = (0.785 * Math.pow(parseFloat(text), 2)).toFixed(2)
-                await bot.sendMessage(chatId, `Результат: <b>${S}</b> мм^2\nВведіть діаметр(мм):`, {parse_mode: "HTML",})
+                await bot.sendMessage(chatId, `Результат: <b>${S}</b> мм^2`, parse_mode)
             } else {
                 await bot.sendMessage(chatId, "Введіть діаметр(мм):", backButton);
                 toggle = 1
             }
         }
-        if (text === "Вибір кабеля по потужності" || toggle === 2) {
+        if (text === "Вибір кабеля по потужності ⚡" || toggle === 2) {
             if (toggle === 2) {
                 let data = await accessSpreadsheet();
                 for (let row = 0; row < data[0].length; row++) {
                     if (parseFloat(data[0][row]["Перетин струмопровідної жили мм^2"]) === getNumber(arr, parseFloat(text))) {
-                        await bot.sendMessage(chatId, `🖋Результат до <b>${data[1]}</b> кабелю.\n✅<b>Для 220В:</b>\n👉Струм - ${data[0][row]['Напруга 220В']} A;\n👉Потужність - ${data[0][row]._rawData[2]} кВт.\n✅<b>Для 380В:</b>\n👉Струм - ${data[0][row]['Напруга 380В']} A;\n👉Потужність - ${data[0][row]._rawData[4]} кВт.`, {parse_mode: "HTML"});
+                        await bot.sendMessage(chatId, `🖋Результат до <b>${data[1]}</b> кабелю.\n✅<b>Для 220В:</b>\n👉Струм - ${data[0][row]['Напруга 220В']} A;\n👉Потужність - ${data[0][row]._rawData[2]} кВт.\n✅<b>Для 380В:</b>\n👉Струм - ${data[0][row]['Напруга 380В']} A;\n👉Потужність - ${data[0][row]._rawData[4]} кВт.`, parse_mode);
                         arr = [];
                         break;
                     }
@@ -56,14 +60,51 @@ const start = async () => {
                 await bot.sendMessage(chatId, "Виберіть матеріал кабелю:", choiceElementButtons);
             }
         }
+        if (text === "Калькулятор 🧮" || toggle === 3) {
+            if (toggle === 3) {
+                if (choiceConvert === 1) {
+                    const A = ((parseFloat(text) * 1000) / U).toFixed(3)
+                    await bot.sendMessage(chatId, `🟢Результат переводу: <b>${A}</b> A`, parse_mode)
+                }
+                else if (choiceConvert === 2) {
+                    const W = ((parseFloat(text) * U) / 1000).toFixed(3)
+                    await bot.sendMessage(chatId, `🟢Результат переводу: <b>${W}</b> кВт`, parse_mode)
+                }
+            } else {
+                await bot.sendMessage(chatId, "Виберіть варіант переводу:", choiceConvertButtons)
+            }
+        }
     });
     bot.on("callback_query", function onCallbackQuery(callbackQuery) {
-        toggle = 2
         if (callbackQuery.data === "Cu") {
+            toggle = 2
             return bot.sendMessage(callbackQuery.message.chat.id, "Введіть переріз кабелю в мм^2:", backButton);
         } else if (callbackQuery.data === "Al") {
+            toggle = 2
             material = 1
             return bot.sendMessage(callbackQuery.message.chat.id, "Введіть переріз кабелю в мм^2:", backButton);
+        }
+        else if (callbackQuery.data === "kW to A") {
+            choiceConvert = 1
+            return bot.sendMessage(callbackQuery.message.chat.id, "Виберіть напругу струму(В):", choiceVoltageButtons);
+        }
+        else if (callbackQuery.data === "A to kW") {
+            choiceConvert = 2
+            return bot.sendMessage(callbackQuery.message.chat.id, "Виберіть напругу струму(В):", choiceVoltageButtons);
+        }
+        else if (callbackQuery.data === "230") {
+            toggle = 3
+            U = parseInt(callbackQuery.data)
+            if (choiceConvert === 1) {
+                return bot.sendMessage(callbackQuery.message.chat.id, "Введіть значення кВт:", backButton);
+            }
+            else if (choiceConvert === 2) {
+                return bot.sendMessage(callbackQuery.message.chat.id, "Введіть значення A:", backButton);
+            }
+        }
+        else if (callbackQuery.data === "400") {
+            toggle = 3
+            U = parseInt(callbackQuery.data)
         }
     });
 }
--- options.js
+++ options.js
@@ -1,7 +1,8 @@
 
 export const keyboardMainMenu = {
     reply_markup: JSON.stringify({
-        keyboard: [["Переріз кабеля", "Вибір кабеля по потужності"]],
+        keyboard: [["Переріз кабеля ➰", "Вибір кабеля по потужності ⚡"],
+        ["Калькулятор 🧮", "Вибір автомата ⚙"]],
         resize_keyboard: true,
         one_time_keyboard: true
     })
@@ -18,7 +19,24 @@ export const choiceElementButtons = {
 
 export const backButton = {
     "reply_markup": {
-        "keyboard": [["Назад"]],
-        "resize_keyboard": true
+        "keyboard": [["Назад👈"]],
+        "resize_keyboard": true,
+        "one_time_keyboard": true
     },
+};
+
+export const choiceConvertButtons = {
+    reply_markup: JSON.stringify({
+        inline_keyboard: [
+            [{text: 'кВт ➡ А', callback_data: 'kW to A'}, {text: 'А ➡ кВт', callback_data: 'A to kW'}],
+        ]
+    })
+};
+
+export const choiceVoltageButtons = {
+    reply_markup: JSON.stringify({
+        inline_keyboard: [
+            [{text: '230В', callback_data: '230'}, {text: '400В', callback_data: '400'}],
+        ]
+    })
 }
