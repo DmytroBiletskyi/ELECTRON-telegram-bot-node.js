7067a2b61be66ee3b7afc12f682266914480f51a
Dima <babaha3000@gmail.com>
2023-02-08 17:39:35 +0200

    del log

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ const start = async () => {
 
 
     bot.on("message", async (msg) => {
-        console.log('1', msg.text)
         const chatId = msg.chat.id;
         const text = msg.text;
 
@@ -35,9 +34,7 @@ const start = async () => {
             await bot.sendMessage(chatId, "Обери наступну дію:", keyboardMainMenu)
         }
         if (text === "Переріз кабеля" || toggle === 1) {
-            console.log('2', msg.text)
             if (toggle === 1) {
-                console.log(toggle)
                 let S = (0.785 * Math.pow(parseFloat(text), 2)).toFixed(2)
                 await bot.sendMessage(chatId, `Результат: <b>${S}</b> мм^2\nВведіть діаметр(мм):`, {parse_mode: "HTML",})
             } else {
@@ -48,8 +45,6 @@ const start = async () => {
         if (text === "Вибір кабеля по потужності" || toggle === 2) {
             if (toggle === 2) {
                 let data = await accessSpreadsheet();
-                console.log(arr);
-                //console.log(rows)
                 for (let row = 0; row < data[0].length; row++) {
                     if (parseFloat(data[0][row]["Перетин струмопровідної жили мм^2"]) === getNumber(arr, parseFloat(text))) {
                         await bot.sendMessage(chatId, `🖋Результат до <b>${data[1]}</b> кабелю.\n✅<b>Для 220В:</b>\n👉Струм - ${data[0][row]['Напруга 220В']} A;\n👉Потужність - ${data[0][row]._rawData[2]} кВт.\n✅<b>Для 380В:</b>\n👉Струм - ${data[0][row]['Напруга 380В']} A;\n👉Потужність - ${data[0][row]._rawData[4]} кВт.`, {parse_mode: "HTML"});
@@ -57,14 +52,12 @@ const start = async () => {
                         break;
                     }
                 }
-                //await bot.sendMessage(chatId, "Введіть переріз кабелю в мм^2:");
             } else {
                 await bot.sendMessage(chatId, "Виберіть матеріал кабелю:", choiceElementButtons);
             }
         }
     });
     bot.on("callback_query", function onCallbackQuery(callbackQuery) {
-        //console.log(callbackQuery.message)
         toggle = 2
         if (callbackQuery.data === "Cu") {
             return bot.sendMessage(callbackQuery.message.chat.id, "Введіть переріз кабелю в мм^2:", backButton);
@@ -78,14 +71,6 @@ const start = async () => {
 await start()
 
 function addDataToArr(row) {
-    //console.log(data._sheet.headerValues)
-    //console.log(data._rowNumber)
-    // console.log(data._rawData)
-    //console.log(data["Перетин струмопровідної жили мм^2"])
-    //console.log(data["Напруга 220 В"])
-    //console.log(data["Напруга 380 В"])
-    //console.log('--------------------------------------------------')
-    //console.log(row._sheet.headerValues);
     arr.push(parseFloat(row["Перетин струмопровідної жили мм^2"]))
 }
 
@@ -102,10 +87,7 @@ async function accessSpreadsheet() {
 
     await doc.loadInfo(); // loads document properties and worksheets
 
-
-    const sheet = doc.sheetsByIndex[material]; // or use doc.sheetsById[id]
-    //console.log(sheet);
-    //console.log(sheet.rowIndex);
+    const sheet = doc.sheetsByIndex[material];
     const rows = await sheet.getRows({
         offset: 1
     });
